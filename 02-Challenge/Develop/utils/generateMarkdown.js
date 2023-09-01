913addcbb6ce8dd87dcbcf74e37f9249ee3d00ee
troyfoster0324 <colorado_tf@yahoo.com>
2023-08-31 19:06:49 -0600

    added functionality to generate markdown

--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -1,19 +1,39 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ---
 
+  ![${data.license}](https://img.shields.io/badge/license-${data.license}-green)
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
+
+  ## Installation
+  To install the necessary dependencies, run the following command:
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License 
+  This project is licensed under the ${data.license} license.
+
+  ## Contributing
+  ${data.contribution}
+
+  ## Tests
+  To run tests, run the following command:
+  ${data.tests}
+
+  ## Questions
+  If you have any questions about this repo, open an issue or contact me directly at [${data.email}](mailto:${data.email}). You can find more of my work at [${data.username}](https://www.github.com/${data.username}).
 `;
 }
 
