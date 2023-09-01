686dd04fde53dd9c23eaf41c04806af9104e839a
troyfoster0324 <colorado_tf@yahoo.com>
2023-08-31 18:50:17 -0600

    initial commit and added some inputs into java

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,7 +1,48 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username"
+},
+{
+    type: "input",
+    message: "What is your email address?",
+    name: "email"
+},
+{
+    type: "input",
+    message: "What is the name of your project?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "What is the description of your project",
+    name: "description"
+},
+{
+    type: "input",
+    message: "Installation insturctions?",
+    name: "instructions"
+},
+{
+    type: "input",
+    message: "What is the usage of your program?",
+    name: "usage"
+},
+{
+    type: "input",
+    message: "Who contributed to this project?",
+    name: "contributors"
+}
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
