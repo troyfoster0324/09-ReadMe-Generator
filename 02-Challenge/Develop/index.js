578e6f9d5aedd84aa35cc11569ab1b843ef287f5
troyfoster0324 <colorado_tf@yahoo.com>
2023-08-31 18:55:30 -0600

    finished index js file

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -38,17 +38,44 @@ const questions = [
     type: "input",
     message: "Who contributed to this project?",
     name: "contributors"
+},
+{
+    type: "input",
+    message: "What command should be run for testing?",
+    name: "tests"
+},
+{
+    type: "list",
+    message: "What kind of license should your project have?",
+    name: "license",
+    choices: [
+        "MIT",
+        "APACHE",
+        "GPL",
+        "BSD",
+        "None"
+    ]
 }
-
-
-
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    let content = generateMarkdown(data);
+    fs.writeFile(fileName, content, function (error) {
+        if (error) {
+            return console.log(error)
+        }
+        console.log('success')
+    });
+}
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function (data) {
+        var fileName = 'README.md';
+        writeToFile(fileName, data)
+    });
+}
 
 // Function call to initialize app
 init();
